37cb480f2aeee03e54ecb67e74961a1c33ef5f09
Khushi Shiroya <98022334+khushi75@users.noreply.github.com>
2023-08-01 17:09:15 +0530

    day2

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Booklist = () => {
-  return <div>Booklist</div>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <div>Here is a Booklist.</div>
+      <button onClick={handleClick}> Go to Home</button>
+    </>
+  );
 };
 
 export default Booklist;
